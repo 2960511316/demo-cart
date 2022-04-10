3b5c0e22dec5e9cfaaab39621778b0361a4a81bc
dongmingxiang <296051316@qq.com>
2022-04-10 15:16:03 +0800

    demo-cart0.2

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -2,30 +2,77 @@
   <div class="goods-container">
     <!-- 左侧图片 -->
     <div class="thumb">
-      <div class="custom-control custom-checkbox">
+      <div class="custom-control custom-checkbox" >
         <!-- 复选框 -->
-        <input type="checkbox" class="custom-control-input" id="cb1" :checked="true" />
-        <label class="custom-control-label" for="cb1">
+        <input
+          type="checkbox"
+          class="custom-control-input"
+          :id="'cb'+id"
+          :checked="state"
+          @change="stateChange"
+        />
+        <label class="custom-control-label" :for="'cb'+id">
           <!-- 商品的缩略图 -->
-          <img src="../../assets/logo.png" alt="" />
+          <img :src="pic" alt="" />
         </label>
       </div>
     </div>
     <!-- 右侧信息区域 -->
     <div class="goods-info">
       <!-- 商品标题 -->
-      <h6 class="goods-title">商品名称商品名称商品名称商品名称</h6>
+      <h6 class="goods-title">{{ title }}</h6>
       <div class="goods-info-bottom">
         <!-- 商品价格 -->
-        <span class="goods-price">￥0</span>
+        <span class="goods-price">￥ {{ price }} </span>
         <!-- 商品的数量 -->
+        <Counter :num="count" :id="id"></Counter>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Counter from '../Counter/Counter.vue';
+export default {
+  components: { Counter },
+  props: {
+    id: {
+      //  required:true, id为必须属性 没有传过来就报错
+      required: true,
+      type: Number,
+    },
+    title: {
+      default: "",
+      type: String,
+    },
+    pic: {
+      default: "",
+      type: String,
+    },
+    price: {
+      default: "",
+      type: Number,
+    },
+    state: {
+      default: "true",
+      type: Boolean,
+    },
+    count: {
+      default: "",
+      type: Number,
+    },
+  },
+  methods: {
+    stateChange(e) {
+      // console.log(e.target.checked); 商品选中状态
+      // console.log(this.id); 商品id
+      // 触发自定义事件
+      // console.log(e.target.checked);
+      const newState = e.target.checked;
+      this.$emit('state-change', {id: this.id, value: newState});
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
